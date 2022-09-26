a37999d5a601de7b56c28fb21412ec718853ba61
Dharunya <88583525+dharunya0609@users.noreply.github.com>
2022-09-26 20:45:32 +0530

    Create Copy.java

---
+++ Copy.java
@@ -0,0 +1,24 @@
+// Iterative Java Program to copy one String
+// to another.
+class Copy
+{
+     
+// Function to copy one string to other
+// assuming that other string has enough
+// space.
+static void myCopy(char s1[], char s2[])
+{
+    int i = 0;
+    for (i = 0; i < s1.length; i++)
+         s2[i] = s1[i];
+}
+ 
+// Driver code
+public static void main(String[] args)
+{
+    char s1[] = "GEEKSFORGEEKS".toCharArray();
+    char s2[] = new char[s1.length];
+    myCopy(s1, s2);
+    System.out.println(String.valueOf(s2));
+}
+}
